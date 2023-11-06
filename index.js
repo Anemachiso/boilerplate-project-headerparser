18e4dca6ba7517d5f772b10b50d17480041f8fc7
Anemachiso <101452814+Anemachiso@users.noreply.github.com>
2023-11-06 19:37:13 +0000

    header parser API microservices

--- index.js
+++ index.js
@@ -24,6 +24,13 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get("/api/whoami", (req, res) => {
+  const ip = req.headers['x-forwarded-for'].split(',')[0];
+  const lang = req.headers["accept-language"];
+  const soft = req.headers["user-agent"]
+  res.json({ ipaddress: ip, language: lang, software: soft });
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
